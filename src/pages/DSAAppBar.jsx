2db8982640ef506ee14add7dba868a299f5bcfda
David Pfahler <pfahler.david@gmail.com>
2019-04-17 08:29:46 +0200

    readded gitmodules. now it works.

---
+++ src/pages/DSAAppBar.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginBottom: theme.spacing.unit * 2
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+});
+
+class DSAAppBar extends React.Component {
+
+  state = {
+    anchorEl: null,
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = (page) => (event) => {
+    this.setState({ anchorEl: null });
+    this.props.onPageChange(page);
+  };
+
+  render() {
+    const { classes, title, pages } = this.props;
+
+    const menuitems = pages.map((p, i) => {
+      return <MenuItem key={i} onClick={this.handleClose(p)}>{p.title}</MenuItem>
+    })
+
+    const { anchorEl } = this.state;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+              aria-owns={anchorEl ? 'simple-menu' : null}
+              aria-haspopup="true"
+              onClick={this.handleClick}>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={this.handleClose}
+            >
+              {menuitems}
+            </Menu>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              {title}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+DSAAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+  pages: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+export default withStyles(styles)(DSAAppBar);
---
+++ src/pages/index.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// material-ui
+import { withStyles } from '@material-ui/core/styles';
+
+// own classes
+import DSAAppBar from './DSAAppBar';
+import SuccessesAndFailuresMain from './successes/SuccessesAndFailuresMain';
+import { DSAGrid, DSAGridRow} from './controls/DSAGrid';
+import DSADescription from './controls/DSADescription';
+import DSAInfoBox from './controls/DSAInfoBox';
+import withRoot from '../withRoot';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  }
+};
+
+const PAGES = [
+  {
+    title: "Successes And Failures",
+    page: <SuccessesAndFailuresMain />
+  },
+  {
+    title: "test2",
+    page: <SuccessesAndFailuresMain />
+  },
+];
+
+class Index extends Component {
+
+  state = {
+    currentpage: PAGES[0]
+  }
+
+  handlePageChange = page => {
+    this.setState({currentpage: page})
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { currentpage } = this.state;
+    return (
+      <div className={classes.root}>
+        <DSAAppBar title={currentpage.title} pages={PAGES} onPageChange={this.handlePageChange}/>
+          {currentpage.page}
+        <footer>
+          <DSAGrid>
+            <DSAGridRow>
+              <DSAInfoBox>
+                <DSADescription caption="Impressum">
+                  Website von David Pfahler.
+                  Dieses Produkt wurde unter Lizenz erstellt. Das Schwarze Auge und sein Logo sowie Aventuria, Dere, Myranor, Riesland, Tharun und Uthuria
+                  und ihre Logos sind eingetragene Marken von Significant GbR in Deutschland, den U.S.A. und anderen Ländern. Ulisses Spiele und sein Logo
+                  sind eingetragene Marken der Ulisses Medien und Spiele Distribution GmbH.
+                  Dieses Werk enthält Material, das durch Ulisses Spiele und/oder andere Autoren urheberrechtlich geschützt ist. Solches Material wird mit
+                  Erlaubnis im Rahmen der Vereinbarung über Gemeinschaftsinhalte für SCRIPTORIUM AVENTURIS verwendet.
+                  Alle anderen Originalmaterialien in diesem Werk sind Copyright 2018 von Moritz Schmid und werden im Rahmen der Vereinbarung über
+                  Gemeinschaftsinhalte für SCRIPTORIUM AVENTURIS veröffentlicht.
+                </DSADescription>
+              </DSAInfoBox>
+            </DSAGridRow>
+          </DSAGrid>
+        </footer>
+      </div>
+    );
+  }
+}
+
+Index.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withRoot(withStyles(styles)(Index));
