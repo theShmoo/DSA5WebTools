5c628a9491ece02e4a9b395fde7daab235748133
David Pfahler <pfahler.david@gmail.com>
2019-05-29 14:18:36 +0200

    Updates

---
+++ precache-manifest.80c94e740939f1404adec91ac8e91224.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506a6ce937e54876d016292b943828be",
+    "url": "/DSA5WebTools/index.html"
+  },
+  {
+    "revision": "146e0d7e645d88f6c04a",
+    "url": "/DSA5WebTools/static/js/2.2d3b4946.chunk.js"
+  },
+  {
+    "revision": "5fbb257956fb5b6c21c5",
+    "url": "/DSA5WebTools/static/js/main.9513ef13.chunk.js"
+  },
+  {
+    "revision": "581e4b9abfde33742ca3",
+    "url": "/DSA5WebTools/static/js/runtime~main.b94b4814.js"
+  }
+]);
